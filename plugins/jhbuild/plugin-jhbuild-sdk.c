ad3a9bdc07e0ac0b23ac93d05c99b0a9ff51ee38
Christian Hergert <chergert@redhat.com>
2024-12-12 16:02:57 -0800

    plugins/jhbuild: implement prepare to build/run

--- plugins/jhbuild/plugin-jhbuild-sdk.c
+++ plugins/jhbuild/plugin-jhbuild-sdk.c
@@ -29,18 +29,77 @@ struct _PluginJhbuildSdk
 
 G_DEFINE_FINAL_TYPE (PluginJhbuildSdk, plugin_jhbuild_sdk, FOUNDRY_TYPE_SDK)
 
-static void
-plugin_jhbuild_sdk_finalize (GObject *object)
+static gboolean
+plugin_jhbuild_sdk_prepare_cb (FoundryProcessLauncher  *launcher,
+                               const char * const      *argv,
+                               const char * const      *env,
+                               const char              *cwd,
+                               FoundryUnixFDMap        *unix_fd_map,
+                               gpointer                 user_data,
+                               GError                 **error)
 {
-  G_OBJECT_CLASS (plugin_jhbuild_sdk_parent_class)->finalize (object);
+  FOUNDRY_ENTRY;
+
+  g_assert (FOUNDRY_IS_MAIN_THREAD ());
+  g_assert (FOUNDRY_IS_PROCESS_LAUNCHER (launcher));
+  g_assert (FOUNDRY_IS_UNIX_FD_MAP (unix_fd_map));
+
+  /* First merge our FDs so we can be sure there are no collisions (there
+   * shouldn't be because we didn't set anything here).
+   */
+  if (!foundry_process_launcher_merge_unix_fd_map (launcher, unix_fd_map, error))
+    FOUNDRY_RETURN (FALSE);
+
+  /* We always take the CWD of the upper layer */
+  foundry_process_launcher_set_cwd (launcher, cwd);
+
+  /* We rewrite the argv to be "jhbuild run ..." */
+  foundry_process_launcher_append_argv (launcher, "jhbuild");
+  foundry_process_launcher_append_argv (launcher, "run");
+
+  /* If there is an environment to deliver, then we want that passed to the
+   * subprocess but not to affect the parent process (jhbuild). So it will now
+   * look something like "jhbuild run env FOO=BAR ..."
+   */
+  if (env != NULL && env[0] != NULL)
+    {
+      foundry_process_launcher_append_argv (launcher, "env");
+      foundry_process_launcher_append_args (launcher, env);
+    }
+
+  /* And now we can add the argv of the upper layers so it might look something
+   * like "jhbuild run env FOO=BAR valgrind env BAR=BAZ my-program"
+   */
+  foundry_process_launcher_append_args (launcher, argv);
+
+  FOUNDRY_RETURN (TRUE);
+}
+
+static DexFuture *
+plugin_jhbuild_sdk_prepare (FoundrySdk             *sdk,
+                            FoundryBuildPipeline   *pipeline,
+                            FoundryProcessLauncher *launcher)
+{
+  g_assert (PLUGIN_IS_JHBUILD_SDK (sdk));
+  g_assert (!pipeline || FOUNDRY_IS_BUILD_PIPELINE (pipeline));
+  g_assert (FOUNDRY_IS_PROCESS_LAUNCHER (launcher));
+
+  foundry_process_launcher_push_host (launcher);
+  foundry_process_launcher_add_minimal_environment (launcher);
+  foundry_process_launcher_push (launcher,
+                                 plugin_jhbuild_sdk_prepare_cb,
+                                 NULL, NULL);
+
+  return dex_future_new_true ();
 }
 
 static void
 plugin_jhbuild_sdk_class_init (PluginJhbuildSdkClass *klass)
 {
-  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+  FoundrySdkClass *sdk_class = FOUNDRY_SDK_CLASS (klass);
 
-  object_class->finalize = plugin_jhbuild_sdk_finalize;
+  sdk_class->prepare_to_build = plugin_jhbuild_sdk_prepare;
+  sdk_class->prepare_to_run = plugin_jhbuild_sdk_prepare;
 }
 
 static void
