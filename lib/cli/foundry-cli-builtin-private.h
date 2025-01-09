fbfb0916a990dc44d0976c84b72f40cf68dc415a
Christian Hergert <chergert@redhat.com>
2025-01-09 14:04:06 -0800

    lib/cli: add command to get build flags of a file

--- lib/cli/foundry-cli-builtin-private.h
+++ lib/cli/foundry-cli-builtin-private.h
@@ -35,6 +35,7 @@ void foundry_cli_builtin_enter               (FoundryCliCommandTree *tree);
 void foundry_cli_builtin_init                (FoundryCliCommandTree *tree);
 void foundry_cli_builtin_device_list         (FoundryCliCommandTree *tree);
 void foundry_cli_builtin_device_switch       (FoundryCliCommandTree *tree);
+void foundry_cli_builtin_pipeline_command    (FoundryCliCommandTree *tree);
 void foundry_cli_builtin_pipeline_info       (FoundryCliCommandTree *tree);
 void foundry_cli_builtin_pipeline_invalidate (FoundryCliCommandTree *tree);
 void foundry_cli_builtin_pipeline_which      (FoundryCliCommandTree *tree);
@@ -61,6 +62,7 @@ _foundry_cli_builtin_register (FoundryCliCommandTree *tree)
   foundry_cli_builtin_init (tree);
   foundry_cli_builtin_device_list (tree);
   foundry_cli_builtin_device_switch (tree);
+  foundry_cli_builtin_pipeline_command (tree);
   foundry_cli_builtin_pipeline_info (tree);
   foundry_cli_builtin_pipeline_invalidate (tree);
   foundry_cli_builtin_pipeline_which (tree);
