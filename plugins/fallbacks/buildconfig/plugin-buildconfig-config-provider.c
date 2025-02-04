72b2eada1fe5cb2163609d2e5137ba81d67bd958
Christian Hergert <chergert@redhat.com>
2025-02-03 16:04:29 -0800

    plugins/fallbacks: load buildconfig from project/user state dirs
    Instead of .buildconfig in your project root, you can instead have a
    .foundry/project/buildconfig or .foundry/user/buildconfig file and they
    will get merged together.

--- plugins/fallbacks/buildconfig/plugin-buildconfig-config-provider.c
+++ plugins/fallbacks/buildconfig/plugin-buildconfig-config-provider.c
@@ -51,6 +51,35 @@ plugin_buildconfig_config_provider_add_default (PluginBuildconfigConfigProvider 
                                         FOUNDRY_CONFIG (config));
 }
 
+static gboolean
+plugin_buildconfig_config_provider_add (PluginBuildconfigConfigProvider *self,
+                                        FoundryContext                  *context,
+                                        GKeyFile                        *key_file)
+{
+  g_auto(GStrv) groups = NULL;
+  gboolean added = FALSE;
+  gsize n_groups;
+
+  groups = g_key_file_get_groups (key_file, &n_groups);
+
+  for (gsize i = 0; i < n_groups; i++)
+    {
+      g_autoptr(FoundryConfig) config = NULL;
+
+      if (strchr (groups[i], '.') != NULL)
+        continue;
+
+      if ((config = plugin_buildconfig_config_new (context, key_file, groups[i])))
+        {
+          foundry_config_provider_config_added (FOUNDRY_CONFIG_PROVIDER (self),
+                                                FOUNDRY_CONFIG (config));
+          added = TRUE;
+        }
+    }
+
+  return added;
+}
+
 static DexFuture *
 plugin_buildconfig_config_provider_load_fiber (gpointer user_data)
 {
@@ -58,6 +87,7 @@ plugin_buildconfig_config_provider_load_fiber (gpointer user_data)
   g_autoptr(FoundryContext) context = NULL;
   g_autoptr(GFile) dot_buildconfig = NULL;
   g_autoptr(GFile) project_dir = NULL;
+  gboolean needs_default = TRUE;
 
   g_assert (PLUGIN_IS_BUILDCONFIG_CONFIG_PROVIDER (self));
 
@@ -65,6 +95,7 @@ plugin_buildconfig_config_provider_load_fiber (gpointer user_data)
   project_dir = foundry_context_dup_project_directory (context);
   dot_buildconfig = g_file_get_child (project_dir, ".buildconfig");
 
+  /* First try to find legacy ".buildconfig" files from Builder */
   if (dex_await_boolean (dex_file_query_exists (dot_buildconfig), NULL))
     {
       g_autoptr(GKeyFile) key_file = NULL;
@@ -72,31 +103,40 @@ plugin_buildconfig_config_provider_load_fiber (gpointer user_data)
 
       if ((key_file = dex_await_boxed (foundry_key_file_new_from_file (dot_buildconfig, 0), &error)))
         {
-          g_auto(GStrv) groups = NULL;
-          gboolean needs_default = TRUE;
-          gsize n_groups;
-
-          groups = g_key_file_get_groups (key_file, &n_groups);
-
-          for (gsize i = 0; i < n_groups; i++)
-            {
-              g_autoptr(FoundryConfig) config = NULL;
-
-              if (strchr (groups[i], '.') != NULL)
-                continue;
-
-              if ((config = plugin_buildconfig_config_new (context, key_file, groups[i])))
-                {
-                  foundry_config_provider_config_added (FOUNDRY_CONFIG_PROVIDER (self),
-                                                        FOUNDRY_CONFIG (config));
-                  needs_default = FALSE;
-                }
-            }
-
-          if (!needs_default)
-            return dex_future_new_true ();
+          if (plugin_buildconfig_config_provider_add (self, context, key_file))
+            needs_default = FALSE;
         }
     }
+
+  /* Now try to load buildconfig files that are merged between project/user
+   * directories (and thus shippable with the project).
+   */
+    {
+      g_autoptr(GKeyFile) key_file = NULL;
+      g_autoptr(GError) error = NULL;
+      g_autoptr(GStrvBuilder) builder = g_strv_builder_new ();
+      g_autoptr(GFile) state_dir = foundry_context_dup_state_directory (context);
+      g_autoptr(GFile) state_pdir = g_file_get_child (state_dir, "project");
+      g_autoptr(GFile) state_udir = g_file_get_child (state_dir, "user");
+      g_auto(GStrv) search_dirs = NULL;
+
+      g_strv_builder_add (builder, g_file_peek_path (state_pdir));
+      g_strv_builder_add (builder, g_file_peek_path (state_udir));
+
+      search_dirs = g_strv_builder_end (builder);
+
+      if ((key_file = dex_await_boxed (foundry_key_file_new_merged ((const char * const *)search_dirs,
+                                                                    "buildconfig",
+                                                                    0),
+                                       &error)))
+        {
+          if (plugin_buildconfig_config_provider_add (self, context, key_file))
+            needs_default = FALSE;
+        }
+    }
+
+  if (!needs_default)
+    return dex_future_new_true ();
 
   plugin_buildconfig_config_provider_add_default (self);
 
