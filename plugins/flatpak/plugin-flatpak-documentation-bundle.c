68f6b63c7ac99c45b158081b609a10023a00e8ad
Christian Hergert <chergert@redhat.com>
2025-04-16 22:54:06 -0700

    plugins/flatpak: rough draft implementation of documentation bundles

---
+++ plugins/flatpak/plugin-flatpak-documentation-bundle.c
@@ -0,0 +1,172 @@
+/* plugin-flatpak-documentation-bundle.c
+ *
+ * Copyright 2025 Christian Hergert <chergert@redhat.com>
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#include "config.h"
+
+#include "plugin-flatpak-documentation-bundle.h"
+
+struct _PluginFlatpakDocumentationBundle
+{
+  FoundryDocumentationBundle parent_instance;
+  FlatpakInstallation *installation;
+  FlatpakRef *ref;
+  guint installed : 1;
+};
+
+enum {
+  PROP_0,
+  PROP_INSTALLATION,
+  PROP_REF,
+  N_PROPS
+};
+
+G_DEFINE_FINAL_TYPE (PluginFlatpakDocumentationBundle, plugin_flatpak_documentation_bundle, FOUNDRY_TYPE_DOCUMENTATION_BUNDLE)
+
+static GParamSpec *properties[N_PROPS];
+
+static gboolean
+plugin_flatpak_documentation_bundle_get_installed (FoundryDocumentationBundle *bundle)
+{
+  PluginFlatpakDocumentationBundle *self = PLUGIN_FLATPAK_DOCUMENTATION_BUNDLE (bundle);
+
+  return self->installed;
+}
+
+static DexFuture *
+plugin_flatpak_documentation_bundle_install (FoundryDocumentationBundle *bundle,
+                                             FoundryOperation           *operation)
+{
+  return dex_future_new_true ();
+}
+
+static void
+plugin_flatpak_documentation_bundle_dispose (GObject *object)
+{
+  PluginFlatpakDocumentationBundle *self = (PluginFlatpakDocumentationBundle *)object;
+
+  g_clear_object (&self->installation);
+  g_clear_object (&self->ref);
+
+  G_OBJECT_CLASS (plugin_flatpak_documentation_bundle_parent_class)->dispose (object);
+}
+
+static void
+plugin_flatpak_documentation_bundle_get_property (GObject    *object,
+                                                  guint       prop_id,
+                                                  GValue     *value,
+                                                  GParamSpec *pspec)
+{
+  PluginFlatpakDocumentationBundle *self = PLUGIN_FLATPAK_DOCUMENTATION_BUNDLE (object);
+
+  switch (prop_id)
+    {
+    case PROP_INSTALLATION:
+      g_value_set_object (value, self->installation);
+      break;
+
+    case PROP_REF:
+      g_value_set_object (value, self->ref);
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+    }
+}
+
+static void
+plugin_flatpak_documentation_bundle_set_property (GObject      *object,
+                                                  guint         prop_id,
+                                                  const GValue *value,
+                                                  GParamSpec   *pspec)
+{
+  PluginFlatpakDocumentationBundle *self = PLUGIN_FLATPAK_DOCUMENTATION_BUNDLE (object);
+
+  switch (prop_id)
+    {
+    case PROP_INSTALLATION:
+      self->installation = g_value_dup_object (value);
+      break;
+
+    case PROP_REF:
+      self->ref = g_value_dup_object (value);
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+    }
+}
+
+static void
+plugin_flatpak_documentation_bundle_class_init (PluginFlatpakDocumentationBundleClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+  FoundryDocumentationBundleClass *bundle_class = FOUNDRY_DOCUMENTATION_BUNDLE_CLASS (klass);
+
+  object_class->dispose = plugin_flatpak_documentation_bundle_dispose;
+  object_class->get_property = plugin_flatpak_documentation_bundle_get_property;
+  object_class->set_property = plugin_flatpak_documentation_bundle_set_property;
+
+  bundle_class->get_installed = plugin_flatpak_documentation_bundle_get_installed;
+  bundle_class->install = plugin_flatpak_documentation_bundle_install;
+
+  properties[PROP_INSTALLATION] =
+    g_param_spec_object ("installation", NULL, NULL,
+                         FLATPAK_TYPE_INSTALLATION,
+                         (G_PARAM_READWRITE |
+                          G_PARAM_CONSTRUCT_ONLY |
+                          G_PARAM_STATIC_STRINGS));
+
+  properties[PROP_REF] =
+    g_param_spec_object ("ref", NULL, NULL,
+                         FLATPAK_TYPE_REF,
+                         (G_PARAM_READWRITE |
+                          G_PARAM_CONSTRUCT_ONLY |
+                          G_PARAM_STATIC_STRINGS));
+
+  g_object_class_install_properties (object_class, N_PROPS, properties);
+}
+
+static void
+plugin_flatpak_documentation_bundle_init (PluginFlatpakDocumentationBundle *self)
+{
+}
+
+FoundryDocumentationBundle *
+plugin_flatpak_documentation_bundle_new (FoundryContext      *context,
+                                         FlatpakInstallation *installation,
+                                         FlatpakRef          *ref,
+                                         gboolean             installed)
+{
+  PluginFlatpakDocumentationBundle *self;
+
+  g_return_val_if_fail (FOUNDRY_IS_CONTEXT (context), NULL);
+  g_return_val_if_fail (FLATPAK_IS_INSTALLATION (installation), NULL);
+  g_return_val_if_fail (FLATPAK_IS_REF (ref), NULL);
+
+  self = g_object_new (PLUGIN_TYPE_FLATPAK_DOCUMENTATION_BUNDLE,
+                       "context", context,
+                       "installation", installation,
+                       "ref", ref,
+                       NULL);
+
+  self->installed = !!installed;
+
+  return FOUNDRY_DOCUMENTATION_BUNDLE (self);
+}
--- plugins/flatpak/plugin-flatpak-documentation-provider.c
+++ plugins/flatpak/plugin-flatpak-documentation-provider.c
@@ -21,6 +21,7 @@
 #include "config.h"
 
 #include "plugin-flatpak.h"
+#include "plugin-flatpak-documentation-bundle.h"
 #include "plugin-flatpak-documentation-provider.h"
 
 struct _PluginFlatpakDocumentationProvider
@@ -251,6 +252,96 @@ plugin_flatpak_documentation_provider_list_roots (FoundryDocumentationProvider *
   return g_object_ref (G_LIST_MODEL (PLUGIN_FLATPAK_DOCUMENTATION_PROVIDER (provider)->roots));
 }
 
+static gboolean
+match_ref (gconstpointer a,
+           gconstpointer b)
+{
+  FlatpakRef *ref_a = FLATPAK_REF (a);
+  FlatpakRef *ref_b = FLATPAK_REF (b);
+
+  return g_strcmp0 (flatpak_ref_get_name (ref_a),
+                    flatpak_ref_get_name (ref_b)) == 0 &&
+         g_strcmp0 (flatpak_ref_get_arch (ref_a),
+                    flatpak_ref_get_arch (ref_b)) == 0 &&
+         g_strcmp0 (flatpak_ref_get_branch (ref_a),
+                    flatpak_ref_get_branch (ref_b)) == 0;
+}
+
+static DexFuture *
+plugin_flatpak_documentation_provider_list_bundles_fiber (gpointer data)
+{
+  PluginFlatpakDocumentationProvider *self = data;
+  g_autoptr(FoundryContext) context = NULL;
+  g_autoptr(GListStore) store = NULL;
+  g_autoptr(GPtrArray) installations = NULL;
+  g_autoptr(GPtrArray) futures = NULL;
+  g_autoptr(GError) error = NULL;
+
+  g_assert (PLUGIN_IS_FLATPAK_DOCUMENTATION_PROVIDER (self));
+
+  context = foundry_contextual_dup_context (FOUNDRY_CONTEXTUAL (self));
+  store = g_list_store_new (FOUNDRY_TYPE_DOCUMENTATION_BUNDLE);
+
+  if (!(installations = dex_await_boxed (plugin_flatpak_load_installations (), &error)))
+    return dex_future_new_for_error (g_steal_pointer (&error));
+
+  futures = g_ptr_array_new_with_free_func (dex_unref);
+
+  for (guint i = 0; i < installations->len; i++)
+    {
+      FlatpakInstallation *installation = g_ptr_array_index (installations, i);
+
+      g_ptr_array_insert (futures, i, plugin_flatpak_installation_list_refs (context, installation, FLATPAK_QUERY_FLAGS_NONE));
+      g_ptr_array_insert (futures, i*2+1, plugin_flatpak_installation_list_installed_refs (context, installation, FLATPAK_QUERY_FLAGS_NONE));
+    }
+
+  if (futures->len)
+    dex_await (dex_future_allv ((DexFuture **)futures->pdata, futures->len), NULL);
+
+  g_assert (futures->len == (installations->len * 2));
+
+  for (guint i = 0; i < installations->len; i++)
+    {
+      FlatpakInstallation *installation = g_ptr_array_index (installations, i);
+      g_autoptr(GPtrArray) refs = dex_await_boxed (dex_ref (g_ptr_array_index (futures, i)), NULL);
+      g_autoptr(GPtrArray) installed = dex_await_boxed (dex_ref (g_ptr_array_index (futures, installations->len + i)), NULL);
+
+      if (refs == NULL)
+        continue;
+
+      for (guint j = 0; j < refs->len; j++)
+        {
+          FlatpakRef *ref = g_ptr_array_index (refs, j);
+          FlatpakRefKind kind = flatpak_ref_get_kind (ref);
+          const char *name = flatpak_ref_get_name (FLATPAK_REF (ref));
+          g_autoptr(FoundryDocumentationBundle) bundle = NULL;
+          gboolean is_installed = FALSE;
+          guint pos;
+
+          if (kind != FLATPAK_REF_KIND_RUNTIME || !g_str_has_suffix (name, ".Docs"))
+            continue;
+
+          if (installed)
+            is_installed = g_ptr_array_find_with_equal_func (installed, ref, match_ref, &pos);
+
+          bundle = plugin_flatpak_documentation_bundle_new (context, installation, ref, is_installed);
+
+          g_list_store_append (store, bundle);
+        }
+    }
+
+  return dex_future_new_take_object (g_steal_pointer (&store));
+}
+
+static DexFuture *
+plugin_flatpak_documentation_provider_list_bundles (FoundryDocumentationProvider *provider)
+{
+  return dex_scheduler_spawn (NULL, 0,
+                              plugin_flatpak_documentation_provider_list_bundles_fiber,
+                              g_object_ref (provider),
+                              g_object_unref);
+}
+
 static void
 plugin_flatpak_documentation_provider_class_init (PluginFlatpakDocumentationProviderClass *klass)
 {
@@ -259,6 +350,7 @@ plugin_flatpak_documentation_provider_class_init (PluginFlatpakDocumentationProv
   documentation_provider_class->load = plugin_flatpak_documentation_provider_load;
   documentation_provider_class->unload = plugin_flatpak_documentation_provider_unload;
   documentation_provider_class->list_roots = plugin_flatpak_documentation_provider_list_roots;
+  documentation_provider_class->list_bundles = plugin_flatpak_documentation_provider_list_bundles;
 }
 
 static void
