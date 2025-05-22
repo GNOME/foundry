79233347441c85f05ffe1ce1500e17511380e608
Christian Hergert <chergert@redhat.com>
2025-05-22 13:49:24 -0700

    lib/gtk: wire up completion providers using wrapper
    The wrapper implementation still needs to be finished, but this gets the
    basic mechanics in place to call it.

--- lib/gtk/foundry-source-view.c
+++ lib/gtk/foundry-source-view.c
@@ -20,12 +20,16 @@
 
 #include "config.h"
 
+#include <libpeas.h>
+
 #include "foundry-source-buffer.h"
+#include "foundry-source-completion-provider.h"
 #include "foundry-source-view.h"
 
 typedef struct
 {
-  gpointer dummy;
+  FoundrySourceBuffer *buffer;
+  FoundryExtensionSet *completion_addins;
 } FoundrySourceViewPrivate;
 
 enum {
@@ -38,19 +42,140 @@ G_DEFINE_TYPE_WITH_PRIVATE (FoundrySourceView, foundry_source_view, GTK_SOURCE_T
 static GParamSpec *properties[N_PROPS];
 
 static void
-foundry_source_view_notify_buffer (FoundrySourceView *self)
+foundry_source_view_completion_provider_added_cb (FoundryExtensionSet *set,
+                                                  PeasPluginInfo      *plugin_info,
+                                                  GObject             *extension,
+                                                  gpointer             user_data)
 {
+  FoundryCompletionProvider *provider = (FoundryCompletionProvider *)extension;
+  g_autoptr(GtkSourceCompletionProvider) wrapper = NULL;
+  GtkSourceCompletion *completion;
+  FoundrySourceView *self = user_data;
+
+  g_assert (FOUNDRY_IS_EXTENSION_SET (set));
+  g_assert (PEAS_IS_PLUGIN_INFO (plugin_info));
+  g_assert (FOUNDRY_IS_COMPLETION_PROVIDER (provider));
   g_assert (FOUNDRY_IS_SOURCE_VIEW (self));
 
+  g_debug ("Add completion provider \"%s\"", G_OBJECT_TYPE_NAME (provider));
+
+  completion = gtk_source_view_get_completion (GTK_SOURCE_VIEW (self));
+  wrapper = foundry_source_completion_provider_new (provider);
+
+  g_object_set_data_full (G_OBJECT (provider),
+                          "GTK_SOURCE_COMPLETION_PROVIDER",
+                          g_object_ref (wrapper),
+                          g_object_unref);
+
+  gtk_source_completion_add_provider (completion, wrapper);
 }
 
 static void
-foundry_source_view_finalize (GObject *object)
+foundry_source_view_completion_provider_removed_cb (FoundryExtensionSet *set,
+                                                    PeasPluginInfo      *plugin_info,
+                                                    GObject             *extension,
+                                                    gpointer             user_data)
+{
+  FoundryCompletionProvider *provider = (FoundryCompletionProvider *)extension;
+  GtkSourceCompletionProvider *wrapper;
+  GtkSourceCompletion *completion;
+  FoundrySourceView *self = user_data;
+
+  g_assert (FOUNDRY_IS_EXTENSION_SET (set));
+  g_assert (PEAS_IS_PLUGIN_INFO (plugin_info));
+  g_assert (FOUNDRY_IS_COMPLETION_PROVIDER (provider));
+  g_assert (FOUNDRY_IS_SOURCE_VIEW (self));
+
+  g_debug ("Remove completion provider \"%s\"", G_OBJECT_TYPE_NAME (provider));
+
+  completion = gtk_source_view_get_completion (GTK_SOURCE_VIEW (self));
+
+  if ((wrapper = g_object_get_data (G_OBJECT (provider), "GTK_SOURCE_COMPLETION_PROVIDER")))
+    {
+      gtk_source_completion_remove_provider (completion, wrapper);
+      g_object_set_data (G_OBJECT (provider), "GTK_SOURCE_COMPLETION_PROVIDER", NULL);
+    }
+}
+
+static gboolean
+_gtk_source_language_to_id_mapping (GBinding     *binding,
+                                    const GValue *from,
+                                    GValue       *to,
+                                    gpointer      user_data)
+{
+  GtkSourceLanguage *language = g_value_get_object (from);
+  if (language != NULL)
+    g_value_set_string (to, gtk_source_language_get_id (language));
+  return TRUE;
+}
+
+static void
+foundry_source_view_notify_buffer (FoundrySourceView *self)
+{
+  FoundrySourceViewPrivate *priv = foundry_source_view_get_instance_private (self);
+  GtkTextBuffer *text_buffer;
+
+  g_assert (FOUNDRY_IS_SOURCE_VIEW (self));
+
+  text_buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (self));
+
+  if (!FOUNDRY_IS_SOURCE_BUFFER (text_buffer))
+    text_buffer = NULL;
+
+  if (g_set_object (&priv->buffer, FOUNDRY_SOURCE_BUFFER (text_buffer)))
+    {
+      g_clear_object (&priv->completion_addins);
+
+      if (priv->buffer != NULL)
+        {
+          g_autoptr(FoundryContext) context = foundry_source_buffer_dup_context (FOUNDRY_SOURCE_BUFFER (priv->buffer));
+          GtkSourceLanguage *language = gtk_source_buffer_get_language (GTK_SOURCE_BUFFER (priv->buffer));
+          const char *language_id = language ? gtk_source_language_get_id (language) : NULL;
+
+          priv->completion_addins = foundry_extension_set_new (context,
+                                                               peas_engine_get_default (),
+                                                               FOUNDRY_TYPE_COMPLETION_PROVIDER,
+                                                               "Completion-Provider-Languages",
+                                                               language_id);
+          g_object_bind_property_full (priv->buffer, "language",
+                                       priv->completion_addins, "value",
+                                       G_BINDING_SYNC_CREATE,
+                                       _gtk_source_language_to_id_mapping, NULL, NULL, NULL);
+          g_signal_connect_object (priv->completion_addins,
+                                   "extension-added",
+                                   G_CALLBACK (foundry_source_view_completion_provider_added_cb),
+                                   self,
+                                   0);
+          g_signal_connect_object (priv->completion_addins,
+                                   "extension-removed",
+                                   G_CALLBACK (foundry_source_view_completion_provider_removed_cb),
+                                   self,
+                                   0);
+          foundry_extension_set_foreach (priv->completion_addins,
+                                         foundry_source_view_completion_provider_added_cb,
+                                         self);
+        }
+    }
+}
+
+static void
+foundry_source_view_constructed (GObject *object)
 {
   FoundrySourceView *self = (FoundrySourceView *)object;
   FoundrySourceViewPrivate *priv = foundry_source_view_get_instance_private (self);
 
-  G_OBJECT_CLASS (foundry_source_view_parent_class)->finalize (object);
+  G_OBJECT_CLASS (foundry_source_view_parent_class)->constructed (object);
+}
+
+static void
+foundry_source_view_dispose (GObject *object)
+{
+  FoundrySourceView *self = (FoundrySourceView *)object;
+  FoundrySourceViewPrivate *priv = foundry_source_view_get_instance_private (self);
+
+  g_clear_object (&priv->completion_addins);
+
+  G_OBJECT_CLASS (foundry_source_view_parent_class)->dispose (object);
 }
 
 static void
@@ -89,7 +214,8 @@ foundry_source_view_class_init (FoundrySourceViewClass *klass)
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
   GtkTextViewClass *text_view_class = GTK_TEXT_VIEW_CLASS (klass);
 
-  object_class->finalize = foundry_source_view_finalize;
+  object_class->constructed = foundry_source_view_constructed;
+  object_class->dispose = foundry_source_view_dispose;
   object_class->get_property = foundry_source_view_get_property;
   object_class->set_property = foundry_source_view_set_property;
 }
