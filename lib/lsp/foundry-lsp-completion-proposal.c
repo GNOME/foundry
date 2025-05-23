de5e350e3b1c31e8d082f59610663c63361a0bdf
Christian Hergert <chergert@redhat.com>
2025-05-22 18:50:59 -0700

    lib/lsp: wire up icon for completion proposal

--- lib/lsp/foundry-lsp-completion-proposal.c
+++ lib/lsp/foundry-lsp-completion-proposal.c
@@ -24,6 +24,34 @@
 
 #include "foundry-lsp-completion-proposal-private.h"
 
+enum {
+	LSP_COMPLETION_TEXT           = 1,
+	LSP_COMPLETION_METHOD         = 2,
+	LSP_COMPLETION_FUNCTION       = 3,
+	LSP_COMPLETION_CONSTRUCTOR    = 4,
+	LSP_COMPLETION_FIELD          = 5,
+	LSP_COMPLETION_VARIABLE       = 6,
+	LSP_COMPLETION_CLASS          = 7,
+	LSP_COMPLETION_INTERFACE      = 8,
+	LSP_COMPLETION_MODULE         = 9,
+	LSP_COMPLETION_PROPERTY       = 10,
+	LSP_COMPLETION_UNIT           = 11,
+	LSP_COMPLETION_VALUE          = 12,
+	LSP_COMPLETION_ENUM           = 13,
+	LSP_COMPLETION_KEYWORD        = 14,
+	LSP_COMPLETION_SNIPPET        = 15,
+	LSP_COMPLETION_COLOR          = 16,
+	LSP_COMPLETION_FILE           = 17,
+	LSP_COMPLETION_REFERENCE      = 18,
+	LSP_COMPLETION_FOLDER         = 19,
+	LSP_COMPLETION_ENUM_MEMBER    = 20,
+	LSP_COMPLETION_CONSTANT       = 21,
+	LSP_COMPLETION_STRUCT         = 22,
+	LSP_COMPLETION_EVENT          = 23,
+	LSP_COMPLETION_OPERATOR       = 24,
+	LSP_COMPLETION_TYPE_PARAMETER = 25,
+};
+
 struct _FoundryLspCompletionProposal
 {
   FoundryCompletionProposal  parent_instance;
@@ -39,6 +67,45 @@ static char *
 foundry_lsp_completion_proposal_dup_typed_text (FoundryCompletionProposal *proposal)
 {
   return g_strdup (FOUNDRY_LSP_COMPLETION_PROPOSAL (proposal)->label);
+}
+
+static GIcon *
+foundry_lsp_completion_proposal_dup_icon (FoundryCompletionProposal *proposal)
+{
+  FoundryLspCompletionProposal *self = FOUNDRY_LSP_COMPLETION_PROPOSAL (proposal);
+
+  switch (self->kind)
+    {
+    case LSP_COMPLETION_METHOD:
+      return g_themed_icon_new ("lang-method-symbolic");
+
+    case LSP_COMPLETION_CONSTRUCTOR:
+    case LSP_COMPLETION_FUNCTION:
+      return g_themed_icon_new ("lang-function-symbolic");
+
+    case LSP_COMPLETION_VARIABLE:
+      return g_themed_icon_new ("lang-struct-field-symbolic");
+
+    case LSP_COMPLETION_CLASS:
+      return g_themed_icon_new ("lang-class-symbolic");
+
+    case LSP_COMPLETION_PROPERTY:
+      return g_themed_icon_new ("lang-property-symbolic");
+
+    case LSP_COMPLETION_ENUM:
+      return g_themed_icon_new ("lang-enum-symbolic");
+
+    case LSP_COMPLETION_ENUM_MEMBER:
+      return g_themed_icon_new ("lang-constant-symbolic");
+
+    case LSP_COMPLETION_STRUCT:
+      return g_themed_icon_new ("lang-struct-symbolic");
+
+    default:
+      break;
+    }
+
+  return NULL;
 }
 
 static void
@@ -62,6 +129,7 @@ foundry_lsp_completion_proposal_class_init (FoundryLspCompletionProposalClass *k
 
   object_class->finalize = foundry_lsp_completion_proposal_finalize;
 
+  proposal_class->dup_icon = foundry_lsp_completion_proposal_dup_icon;
   proposal_class->dup_typed_text = foundry_lsp_completion_proposal_dup_typed_text;
 }
 
