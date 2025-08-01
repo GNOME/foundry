3b9e810583c974ca5b2b211f6ffb520b2db2e1b1
Christian Hergert <chergert@redhat.com>
2025-08-01 11:47:49 -0700

    testsuite/tools: add display of diagnostics model

--- testsuite/tools/test-build-manager-gtk.c
+++ testsuite/tools/test-build-manager-gtk.c
@@ -8,17 +8,54 @@ static GMainLoop *main_loop;
 static const char *dirpath;
 static FoundryPtyDiagnostics *diagnostics;
 
+static void
+setup_row (GtkSignalListItemFactory *factory,
+           GtkListItem              *item,
+           gpointer                  user_data)
+{
+  gtk_list_item_set_child (item,
+                           g_object_new (GTK_TYPE_LABEL,
+                                         "xalign", 0.f,
+                                         NULL));
+}
+
+static void
+bind_row (GtkSignalListItemFactory *factory,
+          GtkListItem              *item,
+          gpointer                  user_data)
+{
+  FoundryDiagnostic *diagnostic = gtk_list_item_get_item (item);
+  g_autoptr(GFile) file = foundry_diagnostic_dup_file (diagnostic);
+  GtkLabel *label = GTK_LABEL (gtk_list_item_get_child (item));
+  g_autoptr(GString) str = g_string_new (NULL);
+  g_autofree char *message = foundry_diagnostic_dup_message (diagnostic);
+  guint line = foundry_diagnostic_get_line (diagnostic);
+  guint line_offset = foundry_diagnostic_get_line_offset (diagnostic);
+
+  g_string_append (str, g_file_peek_path (file));
+  g_string_append_printf (str, ":%u", line+1);
+  g_string_append_printf (str, ":%u", line_offset+1);
+
+  if (message)
+    g_string_append_printf (str, ": %s", message);
+
+  gtk_label_set_label (label, str->str);
+}
+
 static DexFuture *
 main_fiber (gpointer data)
 {
   g_autoptr(GError) error = NULL;
   g_autoptr(FoundryBuildManager) build_manager = NULL;
+  g_autoptr(GtkListItemFactory) factory = NULL;
+  g_autoptr(GtkSelectionModel) model = NULL;
   g_autoptr(FoundryContext) context = NULL;
   g_autoptr(VtePty) pty = NULL;
   g_autofree char *path = NULL;
   GActionGroup *action_group = NULL;
   GtkScrolledWindow *scroller;
   VteTerminal *terminal;
+  GtkListView *listview;
   GtkWindow *window;
   GtkBox *hbox;
   GtkBox *box;
@@ -84,6 +121,18 @@ main_fiber (gpointer data)
 
   build_manager = foundry_context_dup_build_manager (context);
   foundry_build_manager_set_default_pty (build_manager, pty_fd);
+
+  factory = gtk_signal_list_item_factory_new ();
+  g_signal_connect (factory, "setup", G_CALLBACK (setup_row), NULL);
+  g_signal_connect (factory, "bind", G_CALLBACK (bind_row), NULL);
+
+  model = GTK_SELECTION_MODEL (gtk_no_selection_new (g_object_ref (G_LIST_MODEL (diagnostics))));
+  listview = g_object_new (GTK_TYPE_LIST_VIEW,
+                           "height-request", 200,
+                           "factory", factory,
+                           "model", model,
+                           NULL);
+  gtk_scrolled_window_set_child (scroller, GTK_WIDGET (listview));
 
   g_signal_connect_swapped (window,
                             "close-request",
