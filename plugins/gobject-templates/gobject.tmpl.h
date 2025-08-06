3f994eeed4bb8ffaf5acd39badbe7191c4026c05
Christian Hergert <chergert@redhat.com>
2025-08-06 11:59:19 -0700

    plugins/gobject-templates: check for final type

--- plugins/gobject-templates/gobject.tmpl.h
+++ plugins/gobject-templates/gobject.tmpl.h
@@ -10,6 +10,8 @@ G_BEGIN_DECLS
 
 G_DECLARE_{{if final}}FINAL{{else}}DERIVABLE{{end}}_TYPE ({{Prefix}}{{Class}}, {{prefix_}}_{{class_}}, {{PREFIX}}, {{CLASS}}, {{Parent}})
 
+{{if final}}
 {{Prefix}}{{Class}} *{{prefix_}}_{{class_}}_new (void);
+{{end}}
 
 G_END_DECLS
