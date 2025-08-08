292dcaef3e4292f783548a97e6e78d25bf986627
Christian Hergert <chergert@redhat.com>
2025-08-07 18:18:34 -0700

    plugins/simple-templates: allow .project for project templates
    This will allow us to slowly move over templates from meson-templates into
    a more self contained manner. Though we still need to improve how we can
    handle i18n for those once we can extract strings from the files.

--- plugins/simple-templates/plugin-simple-template-provider.c
+++ plugins/simple-templates/plugin-simple-template-provider.c
@@ -31,7 +31,8 @@ G_DEFINE_FINAL_TYPE (PluginSimpleTemplateProvider, plugin_simple_template_provid
 
 static DexFuture *
 plugin_simple_template_provider_list_code_templates_fiber (GFile          *templates_dir,
-                                                           FoundryContext *context)
+                                                           FoundryContext *context,
+                                                           const char     *suffix)
 {
   g_autoptr(GFileEnumerator) enumerator = NULL;
   g_autoptr(GListStore) store = NULL;
@@ -40,6 +41,7 @@ plugin_simple_template_provider_list_code_templates_fiber (GFile          *templ
 
   g_assert (!templates_dir || G_IS_FILE (templates_dir));
   g_assert (!context || FOUNDRY_IS_CONTEXT (context));
+  g_assert (suffix != NULL);
 
   store = g_list_store_new (FOUNDRY_TYPE_CODE_TEMPLATE);
 
@@ -48,9 +50,15 @@ plugin_simple_template_provider_list_code_templates_fiber (GFile          *templ
       for (guint i = 0; children[i]; i++)
         {
           g_autoptr(FoundryCodeTemplate) template = NULL;
-          g_autofree char *uri = g_strconcat ("resource:///app/devsuite/foundry/templates/", children[i], NULL);
-          g_autoptr(GFile) file = g_file_new_for_uri (uri);
           g_autoptr(GError) parse_error = NULL;
+          g_autoptr(GFile) file = NULL;
+          g_autofree char *uri = NULL;
+
+          if (!g_str_has_suffix (children[i], suffix))
+            continue;
+
+          uri = g_strconcat ("resource:///app/devsuite/foundry/templates/", children[i], NULL);
+          file = g_file_new_for_uri (uri);
 
           if ((template = dex_await_object (foundry_simple_code_template_new (context, file), &parse_error)))
             g_list_store_append (store, template);
@@ -80,8 +88,7 @@ plugin_simple_template_provider_list_code_templates_fiber (GFile          *templ
               g_autoptr(GFile) file = NULL;
               g_autoptr(GError) parse_error = NULL;
 
-              /* Skip dot files */
-              if (name == NULL || name[0] == '.')
+              if (name == NULL || !g_str_has_suffix (name, suffix))
                 continue;
 
               file = g_file_enumerator_get_child (enumerator, info);
@@ -115,9 +122,23 @@ plugin_simple_template_provider_list_code_templates (FoundryTemplateProvider *pr
 
   return foundry_scheduler_spawn (dex_thread_pool_scheduler_get_default (), 0,
                                   G_CALLBACK (plugin_simple_template_provider_list_code_templates_fiber),
-                                  2,
+                                  3,
                                   G_TYPE_FILE, templates_dir,
-                                  FOUNDRY_TYPE_CONTEXT, context);
+                                  FOUNDRY_TYPE_CONTEXT, context,
+                                  G_TYPE_STRING, ".template");
+}
+
+static DexFuture *
+plugin_simple_template_provider_list_project_templates (FoundryTemplateProvider *provider)
+{
+  g_assert (PLUGIN_IS_SIMPLE_TEMPLATE_PROVIDER (provider));
+
+  return foundry_scheduler_spawn (dex_thread_pool_scheduler_get_default (), 0,
+                                  G_CALLBACK (plugin_simple_template_provider_list_code_templates_fiber),
+                                  3,
+                                  G_TYPE_FILE, NULL,
+                                  FOUNDRY_TYPE_CONTEXT, NULL,
+                                  G_TYPE_STRING, ".project");
 }
 
 static void
@@ -126,6 +147,7 @@ plugin_simple_template_provider_class_init (PluginSimpleTemplateProviderClass *k
   FoundryTemplateProviderClass *template_provider_class = FOUNDRY_TEMPLATE_PROVIDER_CLASS (klass);
 
   template_provider_class->list_code_templates = plugin_simple_template_provider_list_code_templates;
+  template_provider_class->list_project_templates = plugin_simple_template_provider_list_project_templates;
 }
 
 static void
