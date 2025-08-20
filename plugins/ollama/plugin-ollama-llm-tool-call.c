dfebc9af1eec30961938ad0131d120d73766dc36
Christian Hergert <chergert@redhat.com>
2025-08-19 18:26:03 -0700

    plugins/ollama: setup parameter marshalling for confirm

--- plugins/ollama/plugin-ollama-llm-tool-call.c
+++ plugins/ollama/plugin-ollama-llm-tool-call.c
@@ -48,6 +48,99 @@ plugin_ollama_llm_tool_call_is_callable (FoundryLlmToolCall *tool_call)
   return self->is_callable;
 }
 
+static gboolean
+marshal_param (GValue     *value,
+               GParamSpec *pspec,
+               JsonNode   *arguments)
+{
+  g_auto(GValue) src_value = G_VALUE_INIT;
+  JsonObject *obj;
+  JsonNode *node;
+  GType type;
+
+  if (!JSON_NODE_HOLDS_OBJECT (arguments))
+    return FALSE;
+
+  if (!(obj = json_node_get_object (arguments)))
+    return FALSE;
+
+  if (!(node = json_object_get_member (obj, pspec->name)))
+    return FALSE;
+
+  type = json_node_get_value_type (node);
+
+  if (type == G_VALUE_TYPE (value))
+    {
+      json_node_get_value (node, value);
+      return TRUE;
+    }
+
+  if (!g_value_type_transformable (type, G_VALUE_TYPE (value)))
+    return FALSE;
+
+  g_value_init (&src_value, type);
+  json_node_get_value (node, &src_value);
+  g_value_transform (&src_value, value);
+
+  return TRUE;
+}
+
+static DexFuture *
+plugin_ollama_llm_tool_call_confirm (FoundryLlmToolCall *call)
+{
+  PluginOllamaLlmToolCall *self = (PluginOllamaLlmToolCall *)call;
+  g_autofree GParamSpec **params = NULL;
+  g_autoptr(GArray) values = NULL;
+  guint n_params;
+
+  dex_return_error_if_fail (PLUGIN_IS_OLLAMA_LLM_TOOL_CALL (self));
+  dex_return_error_if_fail (FOUNDRY_IS_LLM_TOOL (self->tool));
+  dex_return_error_if_fail (self->is_callable);
+
+  self->is_callable = FALSE;
+  g_object_notify (G_OBJECT (self), "is-callable");
+
+  params = foundry_llm_tool_list_parameters (self->tool, &n_params);
+  values = g_array_new (FALSE, TRUE, sizeof (GValue));
+  g_array_set_clear_func (values, (GDestroyNotify) g_value_unset);
+  g_array_set_size (values, n_params);
+
+  for (guint i = 0; i < n_params; i++)
+    {
+      GParamSpec *pspec = params[i];
+      GValue *value = &g_array_index (values, GValue, i);
+
+      g_value_init (value, pspec->value_type);
+
+      if (!marshal_param (value, pspec, self->arguments))
+        return dex_future_new_reject (G_IO_ERROR,
+                                      G_IO_ERROR_INVALID_DATA,
+                                      "Invalid param `%s`", pspec->name);
+    }
+
+  return foundry_llm_tool_call (self->tool,
+                                &g_array_index (values, GValue, 0),
+                                values->len);
+}
+
+static DexFuture *
+plugin_ollama_llm_tool_call_deny (FoundryLlmToolCall *call)
+{
+  PluginOllamaLlmToolCall *self = (PluginOllamaLlmToolCall *)call;
+
+  dex_return_error_if_fail (PLUGIN_IS_OLLAMA_LLM_TOOL_CALL (self));
+
+  if (self->is_callable)
+    {
+      self->is_callable = FALSE;
+      g_object_notify (G_OBJECT (self), "is-callable");
+    }
+
+  return dex_future_new_take_object (
+      foundry_simple_llm_message_new (g_strdup ("user"),
+                                      g_strdup ("You may not call that tool")));
+}
+
 static void
 plugin_ollama_llm_tool_call_finalize (GObject *object)
 {
@@ -69,6 +162,8 @@ plugin_ollama_llm_tool_call_class_init (PluginOllamaLlmToolCallClass *klass)
 
   tool_call_class->dup_title = plugin_ollama_llm_tool_call_dup_title;
   tool_call_class->is_callable = plugin_ollama_llm_tool_call_is_callable;
+  tool_call_class->confirm = plugin_ollama_llm_tool_call_confirm;
+  tool_call_class->deny = plugin_ollama_llm_tool_call_deny;
 }
 
 static void
