c36885648ca8c7f47c3f57310b6d2eabe3ad877e
Christian Hergert <chergert@redhat.com>
2025-09-07 20:04:40 -0700

    libfoundry-adw: add scaffolding for Adwaita library
    This will contain intrastructure to build GNOME applications using
    foundry so that we can avoid duplication for that. Suitable candidates
    for landing things here would be IDE UI abstractions, FoundryInput
    bridges to Adwaita widgets, and more.

---
+++ libfoundry-adw/foundry-workspace.h
@@ -0,0 +1,73 @@
+/* foundry-workspace.h
+ *
+ * Copyright 2025 Christian Hergert <chergert@redhat.com>
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#pragma once
+
+#include <gtk/gtk.h>
+
+#include <foundry.h>
+
+#include "foundry-page.h"
+#include "foundry-panel.h"
+#include "foundry-version-macros.h"
+
+G_BEGIN_DECLS
+
+#define FOUNDRY_TYPE_WORKSPACE (foundry_workspace_get_type())
+
+FOUNDRY_AVAILABLE_IN_1_1
+G_DECLARE_FINAL_TYPE (FoundryWorkspace, foundry_workspace, FOUNDRY, WORKSPACE, GtkWidget)
+
+FOUNDRY_AVAILABLE_IN_1_1
+GtkWidget      *foundry_workspace_new               (void);
+FOUNDRY_AVAILABLE_IN_1_1
+FoundryContext *foundry_workspace_get_context       (FoundryWorkspace *self);
+FOUNDRY_AVAILABLE_IN_1_1
+void            foundry_workspace_set_context       (FoundryWorkspace *self,
+                                                     FoundryContext   *context);
+FOUNDRY_AVAILABLE_IN_1_1
+void            foundry_workspace_add_panel         (FoundryWorkspace *self,
+                                                     FoundryPanel     *panel);
+FOUNDRY_AVAILABLE_IN_1_1
+void            foundry_workspace_remove_panel      (FoundryWorkspace *self,
+                                                     FoundryPanel     *panel);
+FOUNDRY_AVAILABLE_IN_1_1
+void            foundry_workspace_add_page          (FoundryWorkspace *self,
+                                                     FoundryPage      *page);
+FOUNDRY_AVAILABLE_IN_1_1
+void            foundry_workspace_remove_page       (FoundryWorkspace *self,
+                                                     FoundryPage      *page);
+FOUNDRY_AVAILABLE_IN_1_1
+GMenuModel     *foundry_workspace_get_primary_menu  (FoundryWorkspace *self);
+FOUNDRY_AVAILABLE_IN_1_1
+void            foundry_workspace_set_primary_menu  (FoundryWorkspace *self,
+                                                     GMenuModel       *menu);
+FOUNDRY_AVAILABLE_IN_1_1
+GtkWidget      *foundry_workspace_get_title_widget  (FoundryWorkspace *self);
+FOUNDRY_AVAILABLE_IN_1_1
+void            foundry_workspace_set_title_widget  (FoundryWorkspace *self,
+                                                     GtkWidget        *title_widget);
+FOUNDRY_AVAILABLE_IN_1_1
+GtkWidget      *foundry_workspace_get_status_widget (FoundryWorkspace *self);
+FOUNDRY_AVAILABLE_IN_1_1
+void            foundry_workspace_set_status_widget (FoundryWorkspace *self,
+                                                     GtkWidget        *status_widget);
+
+G_END_DECLS
