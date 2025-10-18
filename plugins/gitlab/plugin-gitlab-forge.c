fe45c009cbaee0b564441f6328771c339f64e6d8
Christian Hergert <chergert@redhat.com>
2025-10-17 17:10:33 -0700

    plugins/gitlab: start on skeleton implementation of Gitlab forge

---
+++ plugins/gitlab/plugin-gitlab-forge.c
@@ -0,0 +1,49 @@
+/* plugin-gitlab-forge.c
+ *
+ * Copyright 2025 Christian Hergert
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#include "config.h"
+
+#include "plugin-gitlab-forge.h"
+
+struct _PluginGitlabForge
+{
+  FoundryForge parent_instance;
+};
+
+G_DEFINE_FINAL_TYPE (PluginGitlabForge, plugin_gitlab_forge, FOUNDRY_TYPE_FORGE)
+
+static void
+plugin_gitlab_forge_finalize (GObject *object)
+{
+  G_OBJECT_CLASS (plugin_gitlab_forge_parent_class)->finalize (object);
+}
+
+static void
+plugin_gitlab_forge_class_init (PluginGitlabForgeClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->finalize = plugin_gitlab_forge_finalize;
+}
+
+static void
+plugin_gitlab_forge_init (PluginGitlabForge *self)
+{
+}
