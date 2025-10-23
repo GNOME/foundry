3fa0c1f99636f0a5b6624678f492b83cc521b21d
Christian Hergert <chergert@redhat.com>
2025-10-22 22:31:36 -0700

    wip

--- libfoundry/cli/foundry-cli-builtin-mdoc.c
+++ libfoundry/cli/foundry-cli-builtin-mdoc.c
@@ -170,11 +170,464 @@ scan_for_symbol (FoundryGir *gir,
 }
 
 static void
+print_documentation (FoundryCommandLine *command_line,
+                     FoundryGirNode     *node)
+{
+  guint n_docs;
+  FoundryGirNode **docs = foundry_gir_node_list_children_typed (node, FOUNDRY_GIR_NODE_DOC, &n_docs);
+
+  if (docs != NULL && n_docs > 0)
+    {
+      foundry_command_line_print (command_line, "\n");
+      foundry_command_line_print (command_line, "Documentation:\n");
+
+      for (guint i = 0; i < n_docs; i++)
+        {
+          const char *content = foundry_gir_node_get_content (docs[i]);
+          if (content != NULL)
+            {
+              foundry_command_line_print (command_line, "%s\n", content);
+            }
+        }
+    }
+}
+
+static void
+print_function_signature (FoundryCommandLine *command_line,
+                          FoundryGirNode     *node)
+{
+  const char *name = foundry_gir_node_get_attribute (node, "c:identifier");
+  const char *return_type = NULL;
+  guint n_return_values;
+  /* First find the parameters container */
+  guint n_parameters_containers;
+  FoundryGirNode **parameters_containers = foundry_gir_node_list_children_typed (node, FOUNDRY_GIR_NODE_PARAMETERS, &n_parameters_containers);
+  FoundryGirNode **params = NULL;
+  FoundryGirNode **instance_params = NULL;
+  guint n_params = 0;
+  guint n_instance_params = 0;
+
+  if (parameters_containers != NULL && n_parameters_containers > 0)
+    {
+      /* Get parameters from the first parameters container */
+      params = foundry_gir_node_list_children_typed (parameters_containers[0], FOUNDRY_GIR_NODE_PARAMETER, &n_params);
+      instance_params = foundry_gir_node_list_children_typed (parameters_containers[0], FOUNDRY_GIR_NODE_INSTANCE_PARAMETER, &n_instance_params);
+    }
+  FoundryGirNode **return_values = NULL;
+
+  if (name == NULL)
+    name = foundry_gir_node_get_name (node);
+
+  /* Get return type */
+  return_values = foundry_gir_node_list_children_typed (node, FOUNDRY_GIR_NODE_RETURN_VALUE, &n_return_values);
+  if (return_values != NULL && n_return_values > 0)
+    {
+      guint n_types;
+      FoundryGirNode **types = foundry_gir_node_list_children_typed (return_values[0], FOUNDRY_GIR_NODE_TYPE, &n_types);
+      if (types != NULL && n_types > 0)
+        {
+          return_type = foundry_gir_node_get_attribute (types[0], "c:type");
+        }
+    }
+
+  if (return_type == NULL)
+    return_type = "void";
+
+  foundry_command_line_print (command_line, "%s %s (", return_type, name);
+
+
+  /* Process instance parameters first */
+  gboolean first_param = TRUE;
+  if (instance_params != NULL && n_instance_params > 0)
+    {
+      for (guint i = 0; i < n_instance_params; i++)
+        {
+          const char *param_name = foundry_gir_node_get_name (instance_params[i]);
+          const char *param_type = NULL;
+          guint n_param_types;
+          FoundryGirNode **param_types = foundry_gir_node_list_children_typed (instance_params[i], FOUNDRY_GIR_NODE_TYPE, &n_param_types);
+
+          if (param_types != NULL && n_param_types > 0)
+            {
+              param_type = foundry_gir_node_get_attribute (param_types[0], "c:type");
+            }
+
+          if (!first_param)
+            foundry_command_line_print (command_line, ", ");
+          first_param = FALSE;
+
+          if (param_type != NULL)
+            foundry_command_line_print (command_line, "%s", param_type);
+          else
+            foundry_command_line_print (command_line, "void");
+
+          if (param_name != NULL)
+            foundry_command_line_print (command_line, " %s", param_name);
+        }
+    }
+
+  /* Process regular parameters */
+  if (params != NULL && n_params > 0)
+    {
+      for (guint i = 0; i < n_params; i++)
+        {
+          const char *param_name = foundry_gir_node_get_name (params[i]);
+          const char *param_type = NULL;
+          guint n_param_types;
+          FoundryGirNode **param_types = foundry_gir_node_list_children_typed (params[i], FOUNDRY_GIR_NODE_TYPE, &n_param_types);
+
+          if (param_types != NULL && n_param_types > 0)
+            {
+              param_type = foundry_gir_node_get_attribute (param_types[0], "c:type");
+            }
+
+          if (!first_param)
+            foundry_command_line_print (command_line, ", ");
+          first_param = FALSE;
+
+          if (param_type != NULL)
+            foundry_command_line_print (command_line, "%s", param_type);
+          else
+            foundry_command_line_print (command_line, "void");
+
+          if (param_name != NULL)
+            foundry_command_line_print (command_line, " %s", param_name);
+        }
+    }
+
+  foundry_command_line_print (command_line, ");\n");
+}
+
+static void
+print_class_definition (FoundryCommandLine *command_line,
+                        FoundryGirNode     *node)
+{
+  const char *name = foundry_gir_node_get_attribute (node, "c:type");
+  const char *parent = foundry_gir_node_get_attribute (node, "parent");
+  guint n_methods;
+  guint n_implements;
+  FoundryGirNode **methods = NULL;
+  FoundryGirNode **implements = NULL;
+
+  /* Get the namespace name for prefixing unqualified names */
+  const char *namespace_name = NULL;
+
+  /* Find the namespace node by traversing up the tree */
+  FoundryGirNode *current = node;
+  while (current != NULL)
+    {
+      if (foundry_gir_node_get_node_type (current) == FOUNDRY_GIR_NODE_NAMESPACE)
+        {
+          namespace_name = foundry_gir_node_get_attribute (current, "name");
+          break;
+        }
+      current = foundry_gir_node_get_parent (current);
+    }
+
+  if (name == NULL)
+    name = foundry_gir_node_get_name (node);
+
+  foundry_command_line_print (command_line, "class %s", name);
+
+  if (parent != NULL)
+    {
+      /* If parent name doesn't contain ".", prefix it with namespace */
+      if (namespace_name != NULL && strchr (parent, '.') == NULL)
+        foundry_command_line_print (command_line, " inherits %s.%s", namespace_name, parent);
+      else
+        foundry_command_line_print (command_line, " inherits %s", parent);
+    }
+
+  /* Print implemented interfaces */
+  implements = foundry_gir_node_list_children_typed (node, FOUNDRY_GIR_NODE_IMPLEMENTS, &n_implements);
+  if (implements != NULL && n_implements > 0)
+    {
+      for (guint i = 0; i < n_implements; i++)
+        {
+          const char *interface_name = foundry_gir_node_get_attribute (implements[i], "name");
+          if (interface_name != NULL)
+            {
+              /* If interface name doesn't contain ".", prefix it with namespace */
+              if (namespace_name != NULL && strchr (interface_name, '.') == NULL)
+                {
+                  if (i == 0 && parent == NULL)
+                    foundry_command_line_print (command_line, " implements %s.%s", namespace_name, interface_name);
+                  else if (i == 0)
+                    foundry_command_line_print (command_line, ", implements %s.%s", namespace_name, interface_name);
+                  else
+                    foundry_command_line_print (command_line, ", %s.%s", namespace_name, interface_name);
+                }
+              else
+                {
+                  if (i == 0 && parent == NULL)
+                    foundry_command_line_print (command_line, " implements %s", interface_name);
+                  else if (i == 0)
+                    foundry_command_line_print (command_line, ", implements %s", interface_name);
+                  else
+                    foundry_command_line_print (command_line, ", %s", interface_name);
+                }
+            }
+        }
+    }
+
+  foundry_command_line_print (command_line, ";\n");
+
+  /* Print methods outside the class definition */
+  methods = foundry_gir_node_list_children_typed (node, FOUNDRY_GIR_NODE_METHOD, &n_methods);
+
+
+  if (methods != NULL && n_methods > 0)
+    {
+      foundry_command_line_print (command_line, "\n");
+      for (guint i = 0; i < n_methods; i++)
+        {
+          print_function_signature (command_line, methods[i]);
+        }
+    }
+}
+
+static void
+print_enum_definition (FoundryCommandLine *command_line,
+                       FoundryGirNode     *node)
+{
+  const char *name = foundry_gir_node_get_attribute (node, "c:type");
+  const char *c_name = foundry_gir_node_get_attribute (node, "c:identifier");
+  guint n_members;
+  FoundryGirNode **members = NULL;
+
+  if (name == NULL)
+    name = foundry_gir_node_get_name (node);
+
+  if (c_name != NULL)
+    foundry_command_line_print (command_line, "typedef enum %s %s;\n", c_name, name);
+  else
+    foundry_command_line_print (command_line, "enum %s\n", name);
+
+  foundry_command_line_print (command_line, "{\n");
+
+  /* Print enum members */
+  members = foundry_gir_node_list_children_typed (node, FOUNDRY_GIR_NODE_ENUM_MEMBER, &n_members);
+
+  if (members != NULL && n_members > 0)
+    {
+      for (guint i = 0; i < n_members; i++)
+        {
+          const char *member_name = foundry_gir_node_get_attribute (members[i], "c:identifier");
+          const char *member_value = foundry_gir_node_get_attribute (members[i], "value");
+
+          if (member_name == NULL)
+            member_name = foundry_gir_node_get_name (members[i]);
+
+          foundry_command_line_print (command_line, "  %s", member_name);
+
+          if (member_value != NULL)
+            foundry_command_line_print (command_line, " = %s", member_value);
+
+          foundry_command_line_print (command_line, ",\n");
+        }
+    }
+
+  foundry_command_line_print (command_line, "};\n");
+}
+
+static void
+print_constant_definition (FoundryCommandLine *command_line,
+                           FoundryGirNode     *node)
+{
+  const char *name = foundry_gir_node_get_attribute (node, "c:identifier");
+  const char *type = foundry_gir_node_get_attribute (node, "c:type");
+  const char *value = foundry_gir_node_get_attribute (node, "value");
+
+  if (name == NULL)
+    name = foundry_gir_node_get_name (node);
+
+  if (type != NULL)
+    foundry_command_line_print (command_line, "#define %s", name);
+  else
+    foundry_command_line_print (command_line, "#define %s", name);
+
+  if (value != NULL)
+    foundry_command_line_print (command_line, " %s", value);
+
+  foundry_command_line_print (command_line, "\n");
+}
+
+static void
+print_record_definition (FoundryCommandLine *command_line,
+                         FoundryGirNode     *node)
+{
+  const char *name = foundry_gir_node_get_attribute (node, "c:type");
+  guint n_fields;
+  FoundryGirNode **fields = NULL;
+
+  if (name == NULL)
+    name = foundry_gir_node_get_name (node);
+
+  foundry_command_line_print (command_line, "struct %s\n", name);
+  foundry_command_line_print (command_line, "{\n");
+
+  /* Print fields */
+  fields = foundry_gir_node_list_children_typed (node, FOUNDRY_GIR_NODE_FIELD, &n_fields);
+
+  if (fields != NULL && n_fields > 0)
+    {
+      for (guint i = 0; i < n_fields; i++)
+        {
+          const char *field_name = foundry_gir_node_get_name (fields[i]);
+          const char *field_type = NULL;
+          guint n_field_types;
+          FoundryGirNode **field_types = foundry_gir_node_list_children_typed (fields[i], FOUNDRY_GIR_NODE_TYPE, &n_field_types);
+
+          if (field_types != NULL && n_field_types > 0)
+            {
+              field_type = foundry_gir_node_get_attribute (field_types[0], "c:type");
+            }
+
+          if (field_type != NULL)
+            foundry_command_line_print (command_line, "  %s", field_type);
+          else
+            foundry_command_line_print (command_line, "  void");
+
+          if (field_name != NULL)
+            foundry_command_line_print (command_line, " %s", field_name);
+
+          foundry_command_line_print (command_line, ";\n");
+        }
+    }
+
+  foundry_command_line_print (command_line, "};\n");
+}
+
+static void
 mdoc (FoundryCommandLine *command_line,
       FoundryGirNode     *node)
 {
-  g_print ("Got documentation node: %s\n",
-           foundry_gir_node_get_name (node));
+  FoundryGirNodeType node_type = foundry_gir_node_get_node_type (node);
+  const char *name = foundry_gir_node_get_name (node);
+  const char *full_name = NULL;
+
+  /* Get the full type name based on node type */
+  if (node_type == FOUNDRY_GIR_NODE_FUNCTION ||
+      node_type == FOUNDRY_GIR_NODE_FUNCTION_MACRO ||
+      node_type == FOUNDRY_GIR_NODE_METHOD ||
+      node_type == FOUNDRY_GIR_NODE_VIRTUAL_METHOD ||
+      node_type == FOUNDRY_GIR_NODE_CONSTRUCTOR ||
+      node_type == FOUNDRY_GIR_NODE_NAMESPACE_FUNCTION)
+    {
+      full_name = foundry_gir_node_get_attribute (node, "c:identifier");
+    }
+  else if (node_type == FOUNDRY_GIR_NODE_CLASS ||
+           node_type == FOUNDRY_GIR_NODE_INTERFACE ||
+           node_type == FOUNDRY_GIR_NODE_RECORD ||
+           node_type == FOUNDRY_GIR_NODE_UNION ||
+           node_type == FOUNDRY_GIR_NODE_ENUM ||
+           node_type == FOUNDRY_GIR_NODE_ALIAS ||
+           node_type == FOUNDRY_GIR_NODE_CALLBACK ||
+           node_type == FOUNDRY_GIR_NODE_GLIB_BOXED ||
+           node_type == FOUNDRY_GIR_NODE_GLIB_ERROR_DOMAIN ||
+           node_type == FOUNDRY_GIR_NODE_GLIB_SIGNAL)
+    {
+      full_name = foundry_gir_node_get_attribute (node, "c:type");
+    }
+  else if (node_type == FOUNDRY_GIR_NODE_CONSTANT ||
+           node_type == FOUNDRY_GIR_NODE_PROPERTY ||
+           node_type == FOUNDRY_GIR_NODE_FIELD ||
+           node_type == FOUNDRY_GIR_NODE_ENUM_MEMBER)
+    {
+      full_name = foundry_gir_node_get_attribute (node, "c:identifier");
+    }
+
+  if (full_name == NULL)
+    full_name = name;
+
+  foundry_command_line_print (command_line, "Symbol: %s\n", full_name != NULL ? full_name : "<unnamed>");
+
+  switch (node_type)
+    {
+    case FOUNDRY_GIR_NODE_FUNCTION:
+    case FOUNDRY_GIR_NODE_FUNCTION_MACRO:
+    case FOUNDRY_GIR_NODE_METHOD:
+    case FOUNDRY_GIR_NODE_VIRTUAL_METHOD:
+    case FOUNDRY_GIR_NODE_CONSTRUCTOR:
+    case FOUNDRY_GIR_NODE_NAMESPACE_FUNCTION:
+      foundry_command_line_print (command_line, "Type: Function\n");
+      print_function_signature (command_line, node);
+      break;
+
+    case FOUNDRY_GIR_NODE_CLASS:
+    case FOUNDRY_GIR_NODE_INTERFACE:
+      foundry_command_line_print (command_line, "Type: Class\n");
+      print_class_definition (command_line, node);
+      break;
+
+    case FOUNDRY_GIR_NODE_ENUM:
+      foundry_command_line_print (command_line, "Type: Enum\n");
+      print_enum_definition (command_line, node);
+      break;
+
+    case FOUNDRY_GIR_NODE_CONSTANT:
+      foundry_command_line_print (command_line, "Type: Constant\n");
+      print_constant_definition (command_line, node);
+      break;
+
+    case FOUNDRY_GIR_NODE_RECORD:
+    case FOUNDRY_GIR_NODE_UNION:
+      foundry_command_line_print (command_line, "Type: %s\n",
+                                 node_type == FOUNDRY_GIR_NODE_RECORD ? "Record" : "Union");
+      print_record_definition (command_line, node);
+      break;
+
+    case FOUNDRY_GIR_NODE_ENUM_MEMBER:
+      foundry_command_line_print (command_line, "Type: Enum Member\n");
+      print_constant_definition (command_line, node);
+      break;
+
+    case FOUNDRY_GIR_NODE_ALIAS:
+    case FOUNDRY_GIR_NODE_CALLBACK:
+    case FOUNDRY_GIR_NODE_GLIB_BOXED:
+    case FOUNDRY_GIR_NODE_GLIB_ERROR_DOMAIN:
+    case FOUNDRY_GIR_NODE_GLIB_SIGNAL:
+    case FOUNDRY_GIR_NODE_BITFIELD:
+    case FOUNDRY_GIR_NODE_PROPERTY:
+    case FOUNDRY_GIR_NODE_FIELD:
+      foundry_command_line_print (command_line, "Type: %s\n",
+                                 foundry_gir_node_type_to_string (node_type));
+      break;
+
+    case FOUNDRY_GIR_NODE_UNKNOWN:
+    case FOUNDRY_GIR_NODE_REPOSITORY:
+    case FOUNDRY_GIR_NODE_INCLUDE:
+    case FOUNDRY_GIR_NODE_C_INCLUDE:
+    case FOUNDRY_GIR_NODE_PACKAGE:
+    case FOUNDRY_GIR_NODE_NAMESPACE:
+    case FOUNDRY_GIR_NODE_ARRAY:
+    case FOUNDRY_GIR_NODE_CLASS_METHOD:
+    case FOUNDRY_GIR_NODE_CLASS_VIRTUAL_METHOD:
+    case FOUNDRY_GIR_NODE_CLASS_PROPERTY:
+    case FOUNDRY_GIR_NODE_DOC:
+    case FOUNDRY_GIR_NODE_DOC_PARA:
+    case FOUNDRY_GIR_NODE_DOC_TEXT:
+    case FOUNDRY_GIR_NODE_IMPLEMENTS:
+    case FOUNDRY_GIR_NODE_INSTANCE_PARAMETER:
+    case FOUNDRY_GIR_NODE_PARAMETER:
+    case FOUNDRY_GIR_NODE_PARAMETERS:
+    case FOUNDRY_GIR_NODE_PREREQUISITE:
+    case FOUNDRY_GIR_NODE_RETURN_VALUE:
+    case FOUNDRY_GIR_NODE_SOURCE_POSITION:
+    case FOUNDRY_GIR_NODE_TYPE:
+    case FOUNDRY_GIR_NODE_VARARGS:
+      foundry_command_line_print (command_line, "Type: %s\n",
+                                 foundry_gir_node_type_to_string (node_type));
+      break;
+
+    default:
+      foundry_command_line_print (command_line, "Type: %s\n",
+                                 foundry_gir_node_type_to_string (node_type));
+      break;
+    }
+
+  /* Print documentation */
+  print_documentation (command_line, node);
 }
 
 static int
