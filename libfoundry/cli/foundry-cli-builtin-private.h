b78d09882b5679bde984e8c545935f99eb6c5cd1
Christian Hergert <chergert@redhat.com>
2025-11-01 21:10:00 -0700

    libfoundry/cli: ifdef the grep command

--- libfoundry/cli/foundry-cli-builtin-private.h
+++ libfoundry/cli/foundry-cli-builtin-private.h
@@ -79,7 +79,9 @@ void foundry_cli_builtin_sdk_shell           (FoundryCliCommandTree *tree);
 void foundry_cli_builtin_sdk_switch          (FoundryCliCommandTree *tree);
 void foundry_cli_builtin_sdk_which           (FoundryCliCommandTree *tree);
 void foundry_cli_builtin_search              (FoundryCliCommandTree *tree);
+#ifdef FOUNDRY_FEATURE_GREP
 void foundry_cli_builtin_grep                (FoundryCliCommandTree *tree);
+#endif
 void foundry_cli_builtin_secret_get_api_key  (FoundryCliCommandTree *tree);
 void foundry_cli_builtin_secret_set_api_key  (FoundryCliCommandTree *tree);
 void foundry_cli_builtin_settings_get        (FoundryCliCommandTree *tree);
@@ -143,7 +145,9 @@ _foundry_cli_builtin_register (FoundryCliCommandTree *tree)
   foundry_cli_builtin_forge_switch (tree);
   foundry_cli_builtin_forge_user (tree);
 #endif
+#ifdef FOUNDRY_FEATURE_GREP
   foundry_cli_builtin_grep (tree);
+#endif
   foundry_cli_builtin_guess_language (tree);
   foundry_cli_builtin_init (tree);
 #ifdef FOUNDRY_FEATURE_LLM
