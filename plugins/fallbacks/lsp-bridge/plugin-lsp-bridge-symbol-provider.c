315ec92d96b9a3833f17e933decc284d957cc949
Christian Hergert <chergert@redhat.com>
2025-11-12 14:58:03 -0800

    plugins/lsp-bridge: wire up locating the client

--- plugins/fallbacks/lsp-bridge/plugin-lsp-bridge-symbol-provider.c
+++ plugins/fallbacks/lsp-bridge/plugin-lsp-bridge-symbol-provider.c
@@ -29,13 +29,102 @@ struct _PluginLspBridgeSymbolProvider
 
 G_DEFINE_FINAL_TYPE (PluginLspBridgeSymbolProvider, plugin_lsp_bridge_symbol_provider, FOUNDRY_TYPE_SYMBOL_PROVIDER)
 
-static DexFuture *
-plugin_lsp_bridge_symbol_provider_list_symbols_fiber (FoundrySymbolProvider *self,
-                                                      GFile                 *file,
-                                                      GBytes                *contents)
+static char *
+find_language_for_file (PluginLspBridgeSymbolProvider *self,
+                        GFile                         *file,
+                        GBytes                        *contents)
 {
+  g_autoptr(FoundryFileManager) file_manager = NULL;
+  g_autoptr(FoundryTextManager) text_manager = NULL;
+  g_autoptr(FoundryContext) context = NULL;
+  g_autoptr(GListModel) documents = NULL;
+  g_autofree char *language_id = NULL;
+  guint n_items;
+
+  g_assert (PLUGIN_IS_LSP_BRIDGE_SYMBOL_PROVIDER (self));
+
+  if (!(context = foundry_contextual_dup_context (FOUNDRY_CONTEXTUAL (self))))
+    return NULL;
+
+  if (!(text_manager = foundry_context_dup_text_manager (context)))
+    return NULL;
+
+  if (!(documents = foundry_text_manager_list_documents (text_manager)))
+    return NULL;
+
+  n_items = g_list_model_get_n_items (documents);
+
+  for (guint i = 0; i < n_items; i++)
+    {
+      g_autoptr(FoundryTextDocument) document = g_list_model_get_item (documents, i);
+      g_autoptr(GFile) this_file = foundry_text_document_dup_file (document);
+
+      if (g_file_equal (this_file, file))
+        {
+          g_autoptr(FoundryTextBuffer) buffer = foundry_text_document_dup_buffer (document);
+
+          return foundry_text_buffer_dup_language_id (buffer);
+        }
+    }
+
+  if (!(file_manager = foundry_context_dup_file_manager (context)))
+    return NULL;
+
+  if ((language_id = dex_await_string (foundry_file_manager_guess_language (file_manager, file, NULL, contents), NULL)))
+    return g_steal_pointer (&language_id);
+
+  return NULL;
+}
+
+static FoundryLspClient *
+find_client (PluginLspBridgeSymbolProvider  *self,
+             GFile                          *file,
+             GBytes                         *contents,
+             GError                        **error)
+{
+  g_autoptr(FoundryLspManager) lsp_manager = NULL;
+  g_autoptr(FoundryLspClient) client = NULL;
+  g_autoptr(FoundryContext) context = NULL;
+  g_autofree char *language_id = NULL;
+
   g_assert (PLUGIN_IS_LSP_BRIDGE_SYMBOL_PROVIDER (self));
   g_assert (G_IS_FILE (file));
+
+  if (!(context = foundry_contextual_acquire (FOUNDRY_CONTEXTUAL (self), error)))
+    return NULL;
+
+  if (!(language_id = find_language_for_file (self, file, contents)))
+    {
+      g_set_error (error,
+                   G_IO_ERROR,
+                   G_IO_ERROR_NOT_SUPPORTED,
+                   "No language specified");
+      return NULL;
+    }
+
+  lsp_manager = foundry_context_dup_lsp_manager (context);
+
+  if (!(client = dex_await_object (foundry_lsp_manager_load_client (lsp_manager, language_id), error)))
+    return NULL;
+
+  return g_steal_pointer (&client);
+}
+
+static DexFuture *
+plugin_lsp_bridge_symbol_provider_list_symbols_fiber (PluginLspBridgeSymbolProvider *self,
+                                                      GFile                         *file,
+                                                      GBytes                        *contents)
+{
+  g_autoptr(FoundryLspClient) client = NULL;
+  g_autoptr(GError) error = NULL;
+
+  g_assert (PLUGIN_IS_LSP_BRIDGE_SYMBOL_PROVIDER (self));
+  g_assert (G_IS_FILE (file));
+
+  if (!(client = find_client (self, file, contents, &error)))
+    return dex_future_new_for_error (g_steal_pointer (&error));
+
+  /* TODO: Query client */
 
   return foundry_future_new_not_supported ();
 }
@@ -61,14 +150,22 @@ plugin_lsp_bridge_symbol_provider_list_symbols (FoundrySymbolProvider *self,
 }
 
 static DexFuture *
-plugin_lsp_bridge_symbol_provider_find_symbol_at_fiber (FoundrySymbolProvider *self,
-                                                        GFile                 *file,
-                                                        GBytes                *contents,
-                                                        guint                  line,
-                                                        guint                  line_offset)
+plugin_lsp_bridge_symbol_provider_find_symbol_at_fiber (PluginLspBridgeSymbolProvider *self,
+                                                        GFile                         *file,
+                                                        GBytes                        *contents,
+                                                        guint                          line,
+                                                        guint                          line_offset)
 {
+  g_autoptr(FoundryLspClient) client = NULL;
+  g_autoptr(GError) error = NULL;
+
   g_assert (PLUGIN_IS_LSP_BRIDGE_SYMBOL_PROVIDER (self));
   g_assert (G_IS_FILE (file));
+
+  if (!(client = find_client (self, file, contents, &error)))
+    return dex_future_new_for_error (g_steal_pointer (&error));
+
+  /* TODO: Query client */
 
   return foundry_future_new_not_supported ();
 }
