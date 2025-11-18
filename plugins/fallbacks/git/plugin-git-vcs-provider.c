b3183b278189659ab9c1388851fccc58d7bb5ee9
Christian Hergert <chergert@redhat.com>
2025-11-17 19:01:58 -0800

    plugins/git: implement initialize support for VCS

--- plugins/fallbacks/git/plugin-git-vcs-provider.c
+++ plugins/fallbacks/git/plugin-git-vcs-provider.c
@@ -68,6 +68,7 @@ plugin_git_vcs_provider_load_fiber (gpointer data)
     return dex_future_new_for_error (g_steal_pointer (&error));
 
   foundry_vcs_provider_set_vcs (FOUNDRY_VCS_PROVIDER (self), FOUNDRY_VCS (vcs));
+  g_set_object (&self->vcs, vcs);
 
   return dex_future_new_true ();
 }
@@ -97,6 +98,99 @@ plugin_git_vcs_provider_unload (FoundryVcsProvider *provider)
   return dex_future_new_true ();
 }
 
+static DexFuture *
+plugin_git_vcs_provider_initialize_thread (gpointer data)
+{
+  char *path = data;
+  g_autoptr(git_repository) repository = NULL;
+
+  g_assert (path != NULL);
+
+  if (git_repository_init (&repository, path, 0) != 0)
+    return foundry_git_reject_last_error ();
+
+  return dex_future_new_true ();
+}
+
+static DexFuture *
+plugin_git_vcs_provider_initialize_reload_cb (DexFuture *completed,
+                                              gpointer   user_data)
+{
+  PluginGitVcsProvider *self = user_data;
+
+  g_assert (DEX_IS_FUTURE (completed));
+  g_assert (PLUGIN_IS_GIT_VCS_PROVIDER (self));
+
+  if (self->vcs == NULL)
+    return plugin_git_vcs_provider_load (FOUNDRY_VCS_PROVIDER (self));
+
+  return dex_future_new_true ();
+}
+
+static DexFuture *
+plugin_git_vcs_provider_initialize_set_default_cb (DexFuture *completed,
+                                                   gpointer   user_data)
+{
+  PluginGitVcsProvider *self = user_data;
+  g_autoptr(FoundryContext) context = NULL;
+  g_autoptr(FoundryVcsManager) vcs_manager = NULL;
+  g_autoptr(GError) error = NULL;
+
+  g_assert (DEX_IS_FUTURE (completed));
+  g_assert (PLUGIN_IS_GIT_VCS_PROVIDER (self));
+
+  if (!dex_await (dex_ref (completed), &error))
+    g_warning ("%s", error->message);
+
+  if (self->vcs != NULL)
+    {
+      if (!(context = foundry_contextual_dup_context (FOUNDRY_CONTEXTUAL (self))))
+        return dex_future_new_true ();
+
+      if ((vcs_manager = foundry_context_dup_vcs_manager (context)))
+        foundry_vcs_manager_set_vcs (vcs_manager, FOUNDRY_VCS (self->vcs));
+    }
+
+  return dex_future_new_true ();
+}
+
+static DexFuture *
+plugin_git_vcs_provider_initialize (FoundryVcsProvider *provider)
+{
+  PluginGitVcsProvider *self = (PluginGitVcsProvider *)provider;
+  g_autoptr(FoundryContext) context = NULL;
+  g_autoptr(GFile) project_dir = NULL;
+  g_autofree char *path = NULL;
+  DexFuture *future;
+
+  g_assert (PLUGIN_IS_GIT_VCS_PROVIDER (self));
+
+  if (!(context = foundry_contextual_dup_context (FOUNDRY_CONTEXTUAL (self))))
+    return dex_future_new_true ();
+
+  project_dir = foundry_context_dup_project_directory (context);
+
+  if (!g_file_is_native (project_dir))
+    return dex_future_new_true ();
+
+  path = g_file_get_path (project_dir);
+
+  future = dex_thread_spawn ("[git-initialize]",
+                              plugin_git_vcs_provider_initialize_thread,
+                              g_steal_pointer (&path),
+                              g_free);
+  future = dex_future_finally (future,
+                               plugin_git_vcs_provider_initialize_reload_cb,
+                               g_object_ref (self),
+                               g_object_unref);
+  future = dex_future_finally (future,
+                               plugin_git_vcs_provider_initialize_set_default_cb,
+                               g_object_ref (self),
+                               g_object_unref);
+
+  return g_steal_pointer (&future);
+}
+
 static void
 plugin_git_vcs_provider_class_init (PluginGitVcsProviderClass *klass)
 {
@@ -104,6 +198,7 @@ plugin_git_vcs_provider_class_init (PluginGitVcsProviderClass *klass)
 
   vcs_provider_class->load = plugin_git_vcs_provider_load;
   vcs_provider_class->unload = plugin_git_vcs_provider_unload;
+  vcs_provider_class->initialize = plugin_git_vcs_provider_initialize;
 }
 
 static void
