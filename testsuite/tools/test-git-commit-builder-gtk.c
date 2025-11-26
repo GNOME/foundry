754e12cd0606d757835bf98b3d76ee32b1c9e6e2
Christian Hergert <chergert@redhat.com>
2025-11-26 02:53:45 -0800

    testsuite/tools: show untracked/unstaged/staged deltas
    This still is showing diff text because I don't think I'm going to go
    through the effort of doing the whole ListView way for lines in the test
    application.
    
    Unfortunate amount of code for printing that, but whatever, it's just for
    testing things out.

--- testsuite/tools/test-git-commit-builder-gtk.c
+++ testsuite/tools/test-git-commit-builder-gtk.c
@@ -19,12 +19,279 @@
  */
 
 #include <foundry.h>
-#include <foundry-git-commit-builder.h>
 #include <gtk/gtk.h>
+#include <gtksourceview/gtksource.h>
 
 static GMainLoop *main_loop;
 static const char *project_dir;
 static GFile *project_dir_file = NULL;
+static FoundryGitCommitBuilder *commit_builder = NULL;
+static GtkSourceView *diff_textview = NULL;
+static GtkSourceBuffer *diff_buffer = NULL;
+
+#define DEV_NULL "/dev/null"
+
+typedef struct
+{
+  GFile *file;
+  gboolean is_staged;
+} UpdateDiffState;
+
+static void
+update_diff_state_free (UpdateDiffState *state)
+{
+  if (state == NULL)
+    return;
+
+  g_clear_object (&state->file);
+  g_free (state);
+}
+
+/* In a real app you should probably use listview for
+ * line display so that you can do way more interesting
+ * things than show diff text.
+ */
+static char *
+generate_diff_text (FoundryVcsDelta *delta)
+{
+  g_autoptr(GListStore) hunks = NULL;
+  g_autofree char *old_path = NULL;
+  g_autofree char *new_path = NULL;
+  FoundryVcsDeltaStatus status;
+  g_autoptr(GError) error = NULL;
+  GString *diff_text = g_string_new (NULL);
+
+  old_path = foundry_vcs_delta_dup_old_path (delta);
+  new_path = foundry_vcs_delta_dup_new_path (delta);
+  status = foundry_vcs_delta_get_status (delta);
+
+  /* Print diff header */
+  if (status == FOUNDRY_VCS_DELTA_STATUS_RENAMED)
+    g_string_append_printf (diff_text, "diff --git a/%s b/%s\n",
+                            old_path ? old_path : DEV_NULL,
+                            new_path ? new_path : DEV_NULL);
+  else if (status == FOUNDRY_VCS_DELTA_STATUS_DELETED)
+    g_string_append_printf (diff_text, "diff --git a/%s b/%s\n",
+                            old_path ? old_path : DEV_NULL, DEV_NULL);
+  else if (status == FOUNDRY_VCS_DELTA_STATUS_ADDED)
+    g_string_append_printf (diff_text, "diff --git a/%s b/%s\n",
+                            DEV_NULL, new_path ? new_path : DEV_NULL);
+  else
+    g_string_append_printf (diff_text, "diff --git a/%s b/%s\n",
+                            old_path ? old_path : DEV_NULL,
+                            new_path ? new_path : DEV_NULL);
+
+  if (old_path && new_path && g_strcmp0 (old_path, new_path) != 0)
+    g_string_append_printf (diff_text, "rename from %s\nrename to %s\n",
+                            old_path, new_path);
+
+  /* Get hunks */
+  if (!(hunks = dex_await_object (foundry_vcs_delta_list_hunks (delta), &error)))
+    {
+      g_string_append_printf (diff_text, "Error listing hunks: %s\n", error->message);
+      return g_string_free (diff_text, FALSE);
+    }
+
+  /* Print each hunk */
+  for (guint i = 0; i < g_list_model_get_n_items (G_LIST_MODEL (hunks)); i++)
+    {
+      g_autoptr(FoundryVcsDiffHunk) hunk = NULL;
+      g_autoptr(GListStore) lines = NULL;
+      g_autofree char *header = NULL;
+
+      hunk = g_list_model_get_item (G_LIST_MODEL (hunks), i);
+      header = foundry_vcs_diff_hunk_dup_header (hunk);
+
+      if (header)
+        g_string_append (diff_text, header);
+
+      if (!(lines = dex_await_object (foundry_vcs_diff_hunk_list_lines (hunk), &error)))
+        {
+          g_string_append_printf (diff_text, "Error listing lines: %s\n", error->message);
+          continue;
+        }
+
+      for (guint j = 0; j < g_list_model_get_n_items (G_LIST_MODEL (lines)); j++)
+        {
+          g_autoptr(FoundryVcsDiffLine) line = NULL;
+          FoundryVcsDiffLineOrigin origin;
+          g_autofree char *text = NULL;
+          gboolean has_newline;
+
+          line = g_list_model_get_item (G_LIST_MODEL (lines), j);
+          origin = foundry_vcs_diff_line_get_origin (line);
+          text = foundry_vcs_diff_line_dup_text (line);
+          has_newline = foundry_vcs_diff_line_get_has_newline (line);
+
+          g_string_append_c (diff_text, (char)origin);
+          if (text)
+            g_string_append (diff_text, text);
+          if (!has_newline)
+            g_string_append_c (diff_text, '\n');
+        }
+    }
+
+  return g_string_free (diff_text, FALSE);
+}
+
+static DexFuture *
+load_untracked_file_fiber (gpointer data)
+{
+  g_autoptr(GFile) file = data;
+  g_autoptr(GBytes) bytes = NULL;
+  g_autoptr(GError) error = NULL;
+  const guint8 *data_ptr;
+  gsize len;
+  GtkSourceBuffer *buffer;
+  GtkSourceLanguageManager *lang_manager;
+  GtkSourceLanguage *language;
+  g_autofree char *basename = NULL;
+
+  if (file == NULL)
+    {
+      buffer = GTK_SOURCE_BUFFER (gtk_text_view_get_buffer (GTK_TEXT_VIEW (diff_textview)));
+      gtk_text_buffer_set_text (GTK_TEXT_BUFFER (buffer), "", 0);
+      gtk_source_buffer_set_language (buffer, NULL);
+      return dex_future_new_true ();
+    }
+
+  if (!(bytes = dex_await_boxed (dex_file_load_contents_bytes (file), &error)))
+    {
+      buffer = GTK_SOURCE_BUFFER (gtk_text_view_get_buffer (GTK_TEXT_VIEW (diff_textview)));
+      gtk_text_buffer_set_text (GTK_TEXT_BUFFER (buffer), error ? error->message : "Failed to load file", -1);
+      gtk_source_buffer_set_language (buffer, NULL);
+      return dex_future_new_true ();
+    }
+
+  data_ptr = g_bytes_get_data (bytes, &len);
+
+  if (!g_utf8_validate_len ((const char *)data_ptr, len, NULL))
+    {
+      buffer = GTK_SOURCE_BUFFER (gtk_text_view_get_buffer (GTK_TEXT_VIEW (diff_textview)));
+      gtk_text_buffer_set_text (GTK_TEXT_BUFFER (buffer), "File is not valid UTF-8", -1);
+      gtk_source_buffer_set_language (buffer, NULL);
+      return dex_future_new_true ();
+    }
+
+  buffer = GTK_SOURCE_BUFFER (gtk_text_view_get_buffer (GTK_TEXT_VIEW (diff_textview)));
+  gtk_text_buffer_set_text (GTK_TEXT_BUFFER (buffer), (const char *)data_ptr, len);
+
+  lang_manager = gtk_source_language_manager_get_default ();
+  basename = g_file_get_basename (file);
+  language = gtk_source_language_manager_guess_language (lang_manager, basename, NULL);
+  gtk_source_buffer_set_language (buffer, language);
+
+  return dex_future_new_true ();
+}
+
+static DexFuture *
+update_diff_view_fiber (gpointer data)
+{
+  UpdateDiffState *state = data;
+  g_autoptr(DexFuture) future = NULL;
+  g_autoptr(FoundryVcsDelta) delta = NULL;
+  g_autoptr(GError) error = NULL;
+  g_autofree char *diff_text = NULL;
+  GtkSourceBuffer *buffer;
+  GtkSourceLanguageManager *lang_manager;
+  GtkSourceLanguage *language;
+
+  if (state->file == NULL)
+    {
+      buffer = GTK_SOURCE_BUFFER (gtk_text_view_get_buffer (GTK_TEXT_VIEW (diff_textview)));
+      gtk_text_buffer_set_text (GTK_TEXT_BUFFER (buffer), "", 0);
+      gtk_source_buffer_set_language (buffer, NULL);
+      return dex_future_new_true ();
+    }
+
+  if (state->is_staged)
+    future = foundry_git_commit_builder_load_staged_delta (commit_builder, state->file);
+  else
+    future = foundry_git_commit_builder_load_unstaged_delta (commit_builder, state->file);
+
+  if (!(delta = dex_await_object (dex_ref (future), &error)))
+    {
+      buffer = GTK_SOURCE_BUFFER (gtk_text_view_get_buffer (GTK_TEXT_VIEW (diff_textview)));
+      gtk_text_buffer_set_text (GTK_TEXT_BUFFER (buffer), error ? error->message : "Failed to load delta", -1);
+      gtk_source_buffer_set_language (buffer, NULL);
+      return dex_future_new_true ();
+    }
+
+  diff_text = generate_diff_text (delta);
+  buffer = GTK_SOURCE_BUFFER (gtk_text_view_get_buffer (GTK_TEXT_VIEW (diff_textview)));
+  gtk_text_buffer_set_text (GTK_TEXT_BUFFER (buffer), diff_text ? diff_text : "", -1);
+
+  lang_manager = gtk_source_language_manager_get_default ();
+  language = gtk_source_language_manager_get_language (lang_manager, "diff");
+  gtk_source_buffer_set_language (buffer, language);
+
+  return dex_future_new_true ();
+}
+
+static void
+update_diff_view (GFile    *file,
+                  gboolean  is_staged)
+{
+  UpdateDiffState *state;
+
+  state = g_new0 (UpdateDiffState, 1);
+  state->file = file ? g_object_ref (file) : NULL;
+  state->is_staged = is_staged;
+
+  dex_future_disown (dex_scheduler_spawn (NULL, 0,
+                                          update_diff_view_fiber,
+                                          state,
+                                          (GDestroyNotify) update_diff_state_free));
+}
+
+static void
+on_untracked_activate (GtkListView *listview,
+                       guint         position,
+                       gpointer      user_data)
+{
+  GtkSelectionModel *model;
+  g_autoptr(GFile) file = NULL;
+
+  model = gtk_list_view_get_model (listview);
+  file = g_list_model_get_item (G_LIST_MODEL (model), position);
+
+  if (file != NULL)
+    dex_future_disown (dex_scheduler_spawn (NULL, 0, load_untracked_file_fiber, g_steal_pointer (&file), NULL));
+  else
+    {
+      GtkSourceBuffer *buffer = GTK_SOURCE_BUFFER (gtk_text_view_get_buffer (GTK_TEXT_VIEW (diff_textview)));
+      gtk_text_buffer_set_text (GTK_TEXT_BUFFER (buffer), "", 0);
+      gtk_source_buffer_set_language (buffer, NULL);
+    }
+}
+
+static void
+on_unstaged_activate (GtkListView *listview,
+                      guint         position,
+                      gpointer      user_data)
+{
+  GtkSelectionModel *model;
+  g_autoptr(GFile) file = NULL;
+
+  model = gtk_list_view_get_model (listview);
+  file = g_list_model_get_item (G_LIST_MODEL (model), position);
+
+  update_diff_view (file, FALSE);
+}
+
+static void
+on_staged_activate (GtkListView *listview,
+                    guint         position,
+                    gpointer      user_data)
+{
+  GtkSelectionModel *model;
+  g_autoptr(GFile) file = NULL;
+
+  model = gtk_list_view_get_model (listview);
+  file = g_list_model_get_item (G_LIST_MODEL (model), position);
+
+  update_diff_view (file, TRUE);
+}
 
 static void
 setup_row (GtkSignalListItemFactory *factory,
@@ -52,6 +319,26 @@ bind_row (GtkSignalListItemFactory *factory,
   gtk_label_set_label (label, relative_path);
 }
 
+static void
+update_style_scheme (GtkSettings *settings,
+                     GParamSpec  *pspec,
+                     gpointer     user_data)
+{
+  GtkSourceStyleSchemeManager *scheme_manager;
+  GtkSourceStyleScheme *scheme;
+  gboolean prefer_dark;
+
+  if (diff_buffer == NULL)
+    return;
+
+  g_object_get (settings, "gtk-application-prefer-dark-theme", &prefer_dark, NULL);
+
+  scheme_manager = gtk_source_style_scheme_manager_get_default ();
+  scheme = gtk_source_style_scheme_manager_get_scheme (scheme_manager,
+                                                       prefer_dark ? "Adwaita-dark" : "Adwaita");
+  gtk_source_buffer_set_style_scheme (diff_buffer, scheme);
+}
+
 static DexFuture *
 main_fiber (gpointer data)
 {
@@ -60,7 +347,6 @@ main_fiber (gpointer data)
   g_autoptr(FoundryVcsManager) vcs_manager = NULL;
   g_autoptr(FoundryVcs) vcs = NULL;
   g_autoptr(FoundryGitVcs) git_vcs = NULL;
-  g_autoptr(FoundryGitCommitBuilder) commit_builder = NULL;
   g_autoptr(GtkListItemFactory) factory = NULL;
   g_autoptr(GtkSelectionModel) untracked_model = NULL;
   g_autoptr(GtkSelectionModel) unstaged_model = NULL;
@@ -76,6 +362,10 @@ main_fiber (gpointer data)
   GtkListView *unstaged_listview;
   GtkListView *staged_listview;
   GtkBox *box;
+  GtkPaned *hpaned;
+  GtkScrolledWindow *diff_scroller;
+  GtkSourceView *textview;
+  GtkSourceBuffer *text_buffer;
   GtkWindow *window;
 
   dex_await (foundry_init (), NULL);
@@ -100,14 +390,20 @@ main_fiber (gpointer data)
   project_dir_file = g_file_new_for_path (project_dir);
 
   window = g_object_new (GTK_TYPE_WINDOW,
-                         "default-width", 400,
+                         "default-width", 800,
                          "default-height", 600,
                          NULL);
+
+  hpaned = g_object_new (GTK_TYPE_PANED,
+                         "orientation", GTK_ORIENTATION_HORIZONTAL,
+                         "position", 300,
+                         NULL);
+  gtk_window_set_child (window, GTK_WIDGET (hpaned));
 
   box = g_object_new (GTK_TYPE_BOX,
                       "orientation", GTK_ORIENTATION_VERTICAL,
                       NULL);
-  gtk_window_set_child (window, GTK_WIDGET (box));
+  gtk_paned_set_start_child (hpaned, GTK_WIDGET (box));
 
   factory = gtk_signal_list_item_factory_new ();
   g_signal_connect (factory, "setup", G_CALLBACK (setup_row), NULL);
@@ -125,12 +421,14 @@ main_fiber (gpointer data)
   untracked_listview = g_object_new (GTK_TYPE_LIST_VIEW,
                                      "factory", factory,
                                      "model", untracked_model,
+                                     "single-click-activate", TRUE,
                                      NULL);
   untracked_scroller = g_object_new (GTK_TYPE_SCROLLED_WINDOW,
                                      "vexpand", TRUE,
                                      NULL);
   gtk_scrolled_window_set_child (untracked_scroller, GTK_WIDGET (untracked_listview));
   gtk_box_append (box, GTK_WIDGET (untracked_scroller));
+  g_signal_connect (untracked_listview, "activate", G_CALLBACK (on_untracked_activate), NULL);
 
   gtk_box_append (box, g_object_new (GTK_TYPE_LABEL,
                                      "label", "Unstaged",
@@ -144,12 +442,14 @@ main_fiber (gpointer data)
   unstaged_listview = g_object_new (GTK_TYPE_LIST_VIEW,
                                     "factory", factory,
                                     "model", unstaged_model,
+                                    "single-click-activate", TRUE,
                                     NULL);
   unstaged_scroller = g_object_new (GTK_TYPE_SCROLLED_WINDOW,
                                     "vexpand", TRUE,
                                     NULL);
   gtk_scrolled_window_set_child (unstaged_scroller, GTK_WIDGET (unstaged_listview));
   gtk_box_append (box, GTK_WIDGET (unstaged_scroller));
+  g_signal_connect (unstaged_listview, "activate", G_CALLBACK (on_unstaged_activate), NULL);
 
   gtk_box_append (box, g_object_new (GTK_TYPE_LABEL,
                                      "label", "Staged",
@@ -163,12 +463,50 @@ main_fiber (gpointer data)
   staged_listview = g_object_new (GTK_TYPE_LIST_VIEW,
                                   "factory", factory,
                                   "model", staged_model,
+                                  "single-click-activate", TRUE,
                                   NULL);
   staged_scroller = g_object_new (GTK_TYPE_SCROLLED_WINDOW,
-                                   "vexpand", TRUE,
-                                   NULL);
+                                  "vexpand", TRUE,
+                                  NULL);
   gtk_scrolled_window_set_child (staged_scroller, GTK_WIDGET (staged_listview));
   gtk_box_append (box, GTK_WIDGET (staged_scroller));
+  g_signal_connect (staged_listview, "activate", G_CALLBACK (on_staged_activate), NULL);
+
+  text_buffer = gtk_source_buffer_new (NULL);
+  diff_buffer = text_buffer;
+  textview = GTK_SOURCE_VIEW (gtk_source_view_new_with_buffer (text_buffer));
+  gtk_widget_set_hexpand (GTK_WIDGET (textview), TRUE);
+  gtk_widget_set_vexpand (GTK_WIDGET (textview), TRUE);
+  gtk_text_view_set_editable (GTK_TEXT_VIEW (textview), FALSE);
+  gtk_text_view_set_monospace (GTK_TEXT_VIEW (textview), TRUE);
+  diff_textview = textview;
+
+  {
+    GtkSettings *settings;
+    GtkSourceStyleSchemeManager *scheme_manager;
+    GtkSourceStyleScheme *scheme;
+    gboolean prefer_dark;
+
+    settings = gtk_settings_get_default ();
+    g_object_get (settings, "gtk-application-prefer-dark-theme", &prefer_dark, NULL);
+
+    scheme_manager = gtk_source_style_scheme_manager_get_default ();
+    scheme = gtk_source_style_scheme_manager_get_scheme (scheme_manager,
+                                                         prefer_dark ? "Adwaita-dark" : "Adwaita");
+    gtk_source_buffer_set_style_scheme (text_buffer, scheme);
+
+    g_signal_connect (settings,
+                      "notify::gtk-application-prefer-dark-theme",
+                      G_CALLBACK (update_style_scheme),
+                      NULL);
+  }
+
+  diff_scroller = g_object_new (GTK_TYPE_SCROLLED_WINDOW,
+                                "hexpand", TRUE,
+                                "vexpand", TRUE,
+                                NULL);
+  gtk_scrolled_window_set_child (diff_scroller, GTK_WIDGET (textview));
+  gtk_paned_set_end_child (hpaned, GTK_WIDGET (diff_scroller));
 
   g_signal_connect_swapped (window,
                             "close-request",
